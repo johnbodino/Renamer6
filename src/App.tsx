4f773193324986d8b7511feb771a99351db6a014
johnbodino <john.bodino.ndsdata@gmail.com>
2025-09-04 17:01:21 -0400

    Fix removal of (2 MACHINES) text from pushpin names

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
        let cleanedName = originalName
          .replace(/(?:ATR|TMC|P&B|QUE|ADT)-\d+[A-Z]?\s*/i, '')
          .replace(/\b\d{1,3}-HR\b/gi, '')
-        .replace(/\([^)]*\d+\s*MACHINES?\)[^)]*/gi, '')
-        .replace(/\([^)]*\d+M\d+[^)]*\)/gi, '')
+        .replace(/\(\d+\s+MACHINES?\)/gi, '')
+        .replace(/\(\d+M\d+\)/gi, '')
          .replace(/\s{2,}/g, ' ')
          .trim();
